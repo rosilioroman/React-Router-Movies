402cd3143eecbc97e88689405d19f77a269c8fda
rosilioroman <rosilioroman@gmail.com>
2019-01-28 14:48:04 -0700

    Adjust button styles for better UX

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -45,7 +45,7 @@ export default class Movie extends Component {
     const { title, director, metascore, stars } = this.state.movie;
     return (
       <div className="save-wrapper">
-        <div className="movie-card">
+        <div className="movie-card solo-card">
           <h2>{title}</h2>
           <div className="movie-director">
             Director: <em>{director}</em>
