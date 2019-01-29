b1c7f27e49fc8cc29851e5a880a1ddcc7df0f369
rosilioroman <rosilioroman@gmail.com>
2019-01-28 17:01:12 -0700

    Refactor SavedList to be a stateless/functional component.

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,27 +1,20 @@
 import React, { Component } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
-export default class SavedList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {}
-  }
-
-  render() {
-    return (
-      <div className="saved-list">
-        <h3>Saved Movies:</h3>
-        {this.props.list.map(movie => (
-          <span className="saved-movie">
-            <NavLink to={`/movies/${movie.id}`}>{movie.title}</NavLink>
-          </span>
-        ))}
-        <div className="list-buttons">
-          <Link to="/" className="home-button">Home</Link>
-          <div onClick={this.props.clearList} className="clear-button">Clear</div>
-        </div>
-        
+export default function SavedList(props) {
+  return (
+    <div className="saved-list">
+      <h3>Saved Movies:</h3>
+      {props.list.map(movie => (
+        <span className="saved-movie">
+          <NavLink to={`/movies/${movie.id}`}>{movie.title}</NavLink>
+        </span>
+      ))}
+      <div className="list-buttons">
+        <Link to="/" className="home-button">Home</Link>
+        <div onClick={props.clearList} className="clear-button">Clear</div>
       </div>
-    );
-  }
+      
+    </div>
+  );
 }
